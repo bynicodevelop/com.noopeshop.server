1a231b444dbf965931b3031cbeb4aaf1b80b05db
nico-develop <contact@nico-develop.fr>
2022-06-22 06:43:05 +0200

    Permet l'intégration des notifications dans Firebase

--- functions/index.js
+++ functions/index.js
@@ -1,45 +1,38 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin")
+const _ = require("lodash");
 
 admin.initializeApp();
 
-exports.onUploadProductFile = functions.storage.object().onFinalize(async (object) => { 
-    const fileBucket = object.bucket;
-    const filePath = object.name;
-    const contentType = object.contentType;
+// exports.onUploadProductFile = functions.storage.object().onFinalize(async (object) => { 
+//     const filePath = object.name;
+//     // const contentType = object.contentType;
 
-    console.log({
-        fileBucket,
-        filePath,
-        contentType,
-    });
+//     // let mediaType = "MediaTypeEnum.video";
 
-    let mediaType = "MediaTypeEnum.video";
+//     // if(contentType.startsWith("image/")) {
+//     //     mediaType = "MediaTypeEnum.image";
+//     // }
 
-    if(contentType.startsWith("image/")) {
-        mediaType = "MediaTypeEnum.image";
-    }
+//     const productsRef = await admin
+//         .firestore()
+//         .collection("products")
+//         .where("media", "array-contains", filePath)
+//         .get();
 
-    const productsRef = await admin
-        .firestore()
-        .collection("products")
-        .where("media", "array-contains", filePath)
-        .get();
+//     if(productsRef.empty) {
+//         return;
+//     }
 
-    if(productsRef.empty) {
-        return;
-    }
+//     const productRef = productsRef.docs[0];
 
-    const productRef = productsRef.docs[0];
+//     const createdAt = admin.firestore.FieldValue.serverTimestamp();
 
-    const createdAt = admin.firestore.FieldValue.serverTimestamp();
-
-    await productRef.ref.update({
-        mediaType,
-        createdAt,
-        updatedAt: createdAt,
-    });
-});
+//     await productRef.ref.update({
+//         createdAt,
+//         updatedAt: createdAt,
+//     });
+// });
 
 exports.onProductDeleted = functions.firestore.document("products/{productId}").onDelete(async (snapshot) => { 
     const { media } = snapshot.data();
@@ -90,4 +83,50 @@ exports.onFavoriteDeleted = functions.firestore.document("users/{userId}/favorit
     await productRef.update({
         favoritesCount: admin.firestore.FieldValue.increment(-1)
     });
+})
+
+const getPayload = async (title, body, data) => { 
+    const usersQuerySnapshot = await admin.firestore().collection("users").get();
+
+    const tokens = usersQuerySnapshot.docs.map(doc => {return doc.data().notificationToken});
+
+    const payload = {
+        tokens,
+        notification: {
+            title,
+            body,
+        },
+    };
+
+    if(!_.isEmpty(data)) {
+        payload.data = data;
+    }
+
+    return payload
+}
+
+exports.onNotificationCreated = functions.firestore.document("notifications/{notificationId}").onCreate(async (snapshot) => {
+    const { title, body, data, status } = snapshot.data();
+
+    if(status === "pending") {
+        return;
+    }
+
+    const payload = await getPayload(title, body, data);
+
+    await admin.messaging().sendMulticast(payload);
+})
+
+exports.onNotificationUpdated = functions.firestore.document("notifications/{notificationId}").onUpdate(async (snapshot) => {
+    const { title, body, data, status } = snapshot.after.data();
+
+    if(status === "pending") {
+        return;
+    }
+
+    const payload = await getPayload(title, body, data);
+
+    console.log(payload);
+
+    await admin.messaging().sendMulticast(payload);
 })
